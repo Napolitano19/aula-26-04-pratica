a3aff37c0c47a7bf10ff108377e470bc34d032d3
Matheus Falcão de almeida <135279628+izuki005@users.noreply.github.com>
2024-06-27 22:58:28 -0300

    mais um

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs" }
+  },
+  {
+    languageOptions: { globals: globals.browser }
+  },
+  pluginJs.configs.recommended
 ];
-
