aa6f41d7d5a642bcc8c9de08680b4bcb6be09bac
Matheus Falcão de almeida <135279628+izuki005@users.noreply.github.com>
2024-06-27 23:03:50 -0300

    bora

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,15 @@ import pluginJs from "@eslint/js";
 export default [
   {
     files: ["**/*.js"],
-    languageOptions: { sourceType: "commonjs" }
+    languageOptions: {
+      sourceType: "commonjs",
+      parserOptions: {
+        ecmaVersion: 2023 // Ajuste conforme necessário para a versão ECMAScript que você está utilizando
+      },
+      globals: {
+        __dirname: "readonly" // Permite o uso de __dirname como variável global de leitura
+      }
+    }
   },
   {
     languageOptions: { globals: globals.browser }
